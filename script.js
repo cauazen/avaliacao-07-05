0908ecf80fd7a074fa865c559fc77ebe6ce09c8a
CAUÃ FAGUNDES TRAPP <caua_trapp@estudante.sesisenai.org.br>
2025-05-07 09:03:43 -0300

    Adicionando função para registrar log

---
+++ script.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
+
+function registrarLog(nomeAluno) {
+  const id = uuidv4();
+  const dataHora = new Date().toISOString();
+  const logMessage = `${id} - ${dataHora} - ${nomeAluno}\n`;
+
+  fs.appendFile('logs.txt', logMessage, (err) => {
+    if (err) throw err;
+    console.log('Log registrado com sucesso!');
+  });
+}
+
+module.exports = { registrarLog };
